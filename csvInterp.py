189da884b0dfa0e50e05a985ce2f0f22b3315b4a
t0rb1t <benjamin@abellsmith.net>
2019-02-28 17:39:15 -0600

    more progress on csvInterp.py, added classes

--- csvInterp.py
+++ csvInterp.py
@@ -2,10 +2,22 @@
 
 import csv, sys
 
+class weapons:
+    def __init__(self, name, damage, type, handed, idealR, redWindow):
+        self.name = name
+        self.damage = damage
+        self.type = type
+        self.handed = handed
+        if self.type == 'Ranged':
+            self.ranged(self, idealR, redWindow)
+    def ranged(self, idealR, redWindow)
+        self.idealR = idealR
+        self.redWindow = redWindow
+
 with open('weapon-stats.csv', newline='') as csvfile:
-    weaponRead = csv.DictReader(csvfile)
+    weaponRead = csv.DictReader(csvfile, delimiter=',')
+    filteredWeapons = []
     for i in weaponRead:
         if i['Name'] != '':
-            # print(i)
-            pass
-    exWeap = weaponRead[0]
+            filteredWeapons.append(i)
+    print(filteredWeapons[1]['Name'])
