688a15ba5ac1ce1942e445ad611e5a21c945c5a6
t0rb1t <benjamin@abellsmith.net>
2019-02-28 18:14:32 -0600

    more progress on csvInterp.py

--- csvInterp.py
+++ csvInterp.py
@@ -3,33 +3,41 @@
 import csv, sys
 
 class weapons:
-    def __init__(self, name, damage, wType, handed, idealR, redWindow, defDiff):
+    def __init__(self, name, damage, wType, handed, idealR, redPerMeter, defDiff):
         self.name = name
-        self.damage = damage
         self.wType = wType
         self.handed = 1 if handed == 'TRUE' else 2
         if self.wType == 'Ranged':
-            self.ranged(idealR, redWindow, defDiff)
-    def ranged(self, idealR, redWindow, defDiff):
+            self.ranged(idealR, redPerMeter, defDiff, damage)
+        elif self.wType == 'Melee':
+            self.damage = int(damage[4:])
+        elif self.wType == 'Throwable':
+            pass
+    def ranged(self, idealR, redPerMeter, defDiff, damage):
+        self.damage = int(damage)
         self.idealR = idealR
-        self.redWindow = redWindow
+        self.redPerMeter = eval(redPerMeter)
     def specs(self):
         return self.__dict__
 
-def myCsvRead(filename):
+def myCsvReader(filename):
     with open(filename, newline='') as csvfile:
         weaponRead = csv.DictReader(csvfile, delimiter=',')
-        tempWep = []
         filteredWeapons = []
         for i in weaponRead:
             if i['Name'] != '':
-                tempWep = weapons(i['Name'],i['Damage'],i['Type'],i['Single Handed'],i['Ideal range (meters)'],i['Reduction Window'],i['Default Difficulty'])
-                filteredWeapons.append(tempWep)
-
+                filteredWeapons.append(i)
     return filteredWeapons
-        
+
+def weaponToClass(wepList):
+    newWepList = []
+    for i in wepList:
+        tempWep = weapons(i['Name'],i['Damage'],i['Type'],i['Single Handed'],i['Ideal range (meters)'],i['Reduction Window'],i['Default Difficulty'])
+        newWepList.append(tempWep)
+    return newWepList
 
 if __name__ == '__main__':
-    filteredWeapons = myCsvRead('weapon-stats.csv')
-    for i in filteredWeapons:
+    filteredWeapons = myCsvReader('weapon-stats.csv')
+    classWeapons = weaponToClass(filteredWeapons)
+    for i in classWeapons:
         print(i.specs())
