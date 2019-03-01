03583748d65625dec640fc75419b0badec885fb6
t0rb1t <benjamin@abellsmith.net>
2019-02-28 18:02:21 -0600

    more progress on csvInterp.py

--- csvInterp.py
+++ csvInterp.py
@@ -3,21 +3,33 @@
 import csv, sys
 
 class weapons:
-    def __init__(self, name, damage, type, handed, idealR, redWindow):
+    def __init__(self, name, damage, wType, handed, idealR, redWindow, defDiff):
         self.name = name
         self.damage = damage
-        self.type = type
-        self.handed = handed
-        if self.type == 'Ranged':
-            self.ranged(self, idealR, redWindow)
-    def ranged(self, idealR, redWindow)
+        self.wType = wType
+        self.handed = 1 if handed == 'TRUE' else 2
+        if self.wType == 'Ranged':
+            self.ranged(idealR, redWindow, defDiff)
+    def ranged(self, idealR, redWindow, defDiff):
         self.idealR = idealR
         self.redWindow = redWindow
+    def specs(self):
+        return self.__dict__
 
-with open('weapon-stats.csv', newline='') as csvfile:
-    weaponRead = csv.DictReader(csvfile, delimiter=',')
-    filteredWeapons = []
-    for i in weaponRead:
-        if i['Name'] != '':
-            filteredWeapons.append(i)
-    print(filteredWeapons[1]['Name'])
+def myCsvRead(filename):
+    with open(filename, newline='') as csvfile:
+        weaponRead = csv.DictReader(csvfile, delimiter=',')
+        tempWep = []
+        filteredWeapons = []
+        for i in weaponRead:
+            if i['Name'] != '':
+                tempWep = weapons(i['Name'],i['Damage'],i['Type'],i['Single Handed'],i['Ideal range (meters)'],i['Reduction Window'],i['Default Difficulty'])
+                filteredWeapons.append(tempWep)
+
+    return filteredWeapons
+        
+
+if __name__ == '__main__':
+    filteredWeapons = myCsvRead('weapon-stats.csv')
+    for i in filteredWeapons:
+        print(i.specs())
