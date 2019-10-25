5e1bb5dec16da44ebabf4fdfccb59738ad3a5609
T0RB1T <ryan.rizivola@gmail.com>
2019-10-24 23:24:44 -0600

    added pull.sh

--- pull.sh
+++ pull.sh
@@ -2,4 +2,13 @@
 
 git pull
 cd santo/
-zip -r ../santo.msk .
+if zip -rf ../santo.msk . ; then
+	echo there was an archive, so I updated it
+elif [ ! -f ../santo.msk ] ; then
+	zip -r ../santo.msk .
+	echo there was no archive, so I made one
+else
+	echo the archive is already up to date
+fi
+
+
